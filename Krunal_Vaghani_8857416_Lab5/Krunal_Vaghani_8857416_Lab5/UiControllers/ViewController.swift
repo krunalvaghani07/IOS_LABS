6648f5667d67f37213161a8b2ba4f9429eb91ee6
krunal <krunalvaghani07.com>
2023-07-20 12:07:03 -0400

    Lab5 Init

---
+++ Krunal_Vaghani_8857416_Lab5/Krunal_Vaghani_8857416_Lab5/UiControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Krunal_Vaghani_8857416_Lab5
+//
+//  Created by user228677 on 6/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
